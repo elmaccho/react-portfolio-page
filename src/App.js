80958827a50f220c52114558cb6ce9494276b7b1
elmaccho <maciek.chojnacki22@wp.pl>
2023-05-11 22:49:42 +0200

    new

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 
 import { NavBar } from './components/NavBar';
 import { Header } from './components/Header';
+import { AboutMe } from './components/AboutMe';
 
 function App() {
   return (
     <div className="App">
       <NavBar></NavBar>
       <Header></Header>
+      <AboutMe></AboutMe>
     </div>
   );
 }
---
+++ src/components/AboutMe/AboutMe.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './style.css'
+
+function AboutMe(){
+    return (
+        <>
+            <div className="aboutMe-container">
+                
+            </div>
+        
+        </>
+    )
+}
+
+export default AboutMe
