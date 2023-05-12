6482d87f3ff2ab5db5985f0bc1477c38889c9c19
elmaccho <maciek.chojnacki22@wp.pl>
2023-05-12 20:08:41 +0200

    about me

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,11 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
 import './style.css'
 
 function AboutMe(){
+
+    const [isHovered, setIsHovered] = useState(false)
+
+    const handleMouseEnter = () => {
+        setIsHovered(true);
+    }
+
+    const handleMouseLeave = () => {
+        setIsHovered(false);
+    }
+
+
     return (
         <>
             <div className="aboutMe-container">
-                
+                <div className="aboutMe-img">
+                    <img alt="zdjecia nie ma jeszcze"></img>
+                </div>
+                <div className="aboutMe-text">
+                    <h1 className="aboutMe-text-title">
+                        O mnie
+                    </h1>
+                    <p className="aboutMe-info">
+                    Jestem Maciej, 21-letni pasjonat programowania. 
+                    Moja przygoda z Web Developmentem rozpoczęła się półtora roku temu. 
+                    W tym czasie zdobyłem wiele <span className={isHovered ? "skills" : 'skills'} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>umiejętności</span> i rozwinąłem się w wielu językach i technologiach związanych z tworzeniem stron internetowych. 
+                    Potrafię tworzyć responsywne projekty, które umożliwiają użytkownikom korzystanie ze strony na dowolnym urządzeniu.
+                     Mimo braku doświadczenia komercyjnego, jestem w stanie samodzielnie pracować i rozwiązywać problemy.
+                    </p>
+                    <div className={isHovered ? "skill-container active" : "skill-container"}>
+
+                        <div className="skillBar-container">
+
+                            <div className="skillBar-bar">
+                                <span className="skillBar-bar-title">HTML</span>
+                                <div className="skillBar-length html"></div>
+                            </div>
+                            <div className="skillBar-counter">95%</div>
+
+                        </div>
+
+                        <div className="skillBar-container">
+
+                            <div className="skillBar-bar">
+                                <span className="skillBar-bar-title">CSS/SCSS</span>
+                                <div className="skillBar-length css"></div>
+                            </div>
+                            <div className="skillBar-counter">95%</div>
+
+                        </div>
+
+                        <div className="skillBar-container">
+
+                            <div className="skillBar-bar">
+                                <span className="skillBar-bar-title">JavaScript</span>
+                                <div className="skillBar-length js"></div>
+                            </div>
+                            <div className="skillBar-counter">75%</div>
+
+                        </div>
+
+
+                        <div className="skillBar-container">
+
+                            <div className="skillBar-bar">
+                                <span className="skillBar-bar-title">React</span>
+                                <div className="skillBar-length react"></div>
+                            </div>
+                            <div className="skillBar-counter">15%</div>
+
+                        </div>
+
+
+                    </div>
+                </div>
             </div>
         
         </>
